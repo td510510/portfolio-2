bda3cb8a97f12068a8d7ca4a62c60ef52340c043
thinh.dang <thinh.dang@skyblazestudios.com>
2023-03-10 14:47:20 +0700

    update project

--- containers/Skills/index.tsx
+++ containers/Skills/index.tsx
@@ -6,23 +6,49 @@ import JavascriptIcon from '../../public/icons/javascript.svg'
 import ReactJsIcon from '../../public/icons/reactjs.svg'
 import NextJsIcon from '../../public/icons/nextjs.svg'
 import TypescriptIcon from '../../public/icons/typescript.svg'
+import NodeJsIcon from '../../public/icons/nodejs.svg'
 
 const Skills = () => {
   const icons = [
-    <><HtmlIcon /></>,
-    <><CssIcon /></>,
-    <><SassIcon /></>,
-    <><JavascriptIcon /></>,
-    <><TypescriptIcon /></>,
-    <><ReactJsIcon /></>,
-    <><NextJsIcon /></>,
+    {
+      name: <><HtmlIcon /></>,
+      title: 'HTML'
+    },
+    {
+      name: <><CssIcon /></>,
+      title: 'CSS'
+    },
+    {
+      name: <><SassIcon /></>,
+      title: 'Sass'
+    },
+    {
+      name: <><JavascriptIcon /></>,
+      title: 'Javascript'
+    },
+    {
+      name: <><TypescriptIcon /></>,
+      title: 'HTML'
+    },
+    {
+      name: <><ReactJsIcon /></>,
+      title: 'ReactJs'
+    },
+    {
+      name: <><NextJsIcon /></>,
+      title: 'NextJs'
+    },
+    {
+      name: <><NodeJsIcon /></>,
+      title: 'NodeJs'
+    },
   ]
   return (
     <div className={styles.container}>
       <div className={styles.box}>
         {
           icons.map((icon, i) => (
-            <div className={`${styles.icon} cursor-scale`} key={`icon-${i}`}>{icon}</div>
+            <div className={`${styles.icon} cursor-scale`} key={`icon-${i}`} title={icon.title}>{icon.name}</div>
           ))
         }
       </div>
